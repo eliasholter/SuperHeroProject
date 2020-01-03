eda1b81388d7ec4c6afc430cde880ad203d0b022
Elias Holter <eliasholter1@gmail.com>
2020-01-03 12:07:31 -0600

    Working through adding roles

--- SuperHeroProject/Startup.cs
+++ SuperHeroProject/Startup.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
 using Owin;
 using SuperHeroProject.Models;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 [assembly: OwinStartupAttribute(typeof(SuperHeroProject.Startup))]
 namespace SuperHeroProject
@@ -16,6 +21,29 @@ namespace SuperHeroProject
         private void CreateRolesAndUsers()
         {
             ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "eholter1";
+                user.Email = "eliasholter1@gmail.com";
+
+                string userPWD = "imagoofygoober";
+
+                var chkUser = userManager.Create(user, userPWD);
+
+                if (chkUser.Succeeded)
+                {
+                    var result1 = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
         }
     }
 }
